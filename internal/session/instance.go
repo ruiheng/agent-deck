eb63b295c807ae4c0f021e285d128f7bb124fcb6
Gilbert Fine <gilbert.fine@gmail.com>
2026-03-17 15:46:42 +0800

    fix(send): treat visible composer input as staged
    (cherry picked from commit 193565a7d5681a75176afacfc46a95e00b1b0630)

--- internal/session/instance.go
+++ internal/session/instance.go
@@ -2156,9 +2156,13 @@ func (i *Instance) sendMessageWhenReady(message string) error {
 				time.Sleep(verifyDelay)
 
 				unsentPromptDetected := false
+				hasVisibleComposerInput := false
 				if rawContent, captureErr := i.tmuxSession.CapturePaneFresh(); captureErr == nil {
 					content := tmux.StripANSI(rawContent)
 					unsentPromptDetected = send.HasUnsentPastedPrompt(content) || send.HasUnsentComposerPrompt(content, message)
+					if promptBody, hasPrompt := send.CurrentComposerPrompt(content); hasPrompt && send.NormalizePromptText(promptBody) != "" {
+						hasVisibleComposerInput = true
+					}
 				}
 				verifiedStatus, statusErr := i.tmuxSession.GetStatus()
 
@@ -2188,6 +2192,10 @@ func (i *Instance) sendMessageWhenReady(message string) error {
 						}
 					} else {
 						waitingNoMarkerChecks = 0
+						if hasVisibleComposerInput {
+							_ = i.tmuxSession.SendEnter()
+							continue
+						}
 						// We haven't observed any post-send activity yet.
 						// Nudge Enter aggressively in the early window (every
 						// iteration for first 5 retries) then every 2nd iteration.
